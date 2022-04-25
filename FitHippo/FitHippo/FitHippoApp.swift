7236bc6224eb9a1fb6fa8d296badd4746c3bd984
Matthew Bratrsovsky <77427123+mattkbrat@users.noreply.github.com>
2022-04-25 10:51:24 -0600

    Setup project

---
+++ FitHippo/FitHippo/FitHippoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitHippoApp.swift
+//  FitHippo
+//
+//  Created by BACS495 on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct FitHippoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
