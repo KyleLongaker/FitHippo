cf0c920248ee9411a5f6a785064d480c8f4fd872
Matthew Bratrsovsky <77427123+mattkbrat@users.noreply.github.com>
2022-04-28 14:48:01 -0600

    Update FitHippoApp.swift

--- FitHippo/FitHippo/FitHippoApp.swift
+++ FitHippo/FitHippo/FitHippoApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct FitHippoApp: App {
+    @StateObject var user = User()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(user)
         }
     }
 }
